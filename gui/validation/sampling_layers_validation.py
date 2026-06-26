6fc7d3f64188c86724a8fdb3761f11b36c3223ac
LPCZeitouni <ocor@lallemand.com>
2026-06-26 14:16:19 +0200

    Update sampling_layers_validation.py

--- gui/validation/sampling_layers_validation.py
+++ gui/validation/sampling_layers_validation.py
@@ -1,29 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-LallemandGeostatFieldTrialTreatments
-
-A QGIS plugin for agronomic field trial analysis using geostatistical
-(Kriging-based) models to analyze and compare treatment scenarios.
-
----------------------------------------------------------------------
+/***************************************************************************
+A QGIS plugin for agronomic field trial analysis based on geostatistical (Kriging-based) models.
+/***************************************************************************
 Begin        : 2026-02-09
 Copyright    : (C) 2026 Olivier Cor
 Email        : ocor@lallemand.com
 License      : GNU General Public License v3.0 or later (GPLv3+)
-
+/***************************************************************************
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
+/***************************************************************************
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
----------------------------------------------------------------------
+/***************************************************************************
 """
 
 from qgis.PyQt.QtCore import QObject
